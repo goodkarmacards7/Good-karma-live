b1ded649de50fa740bd14c4b9c18093289697649
goodkarmacards7 <goodkarmacards7@gmail.com>
2026-08-13 19:24:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.GOOD_KARMA_CONFIG = {
   sheetId: "1893dQvXUL_8ltl9zem8ygiHIUfo4pzui3q2ceQsmNW4",
   hitsGid: "0",
   statsGid: "1803586008",
-  refreshSeconds: 10,
-  celebrationSeconds: 7,
+  refreshSeconds: 2,
+  celebrationSeconds: 15,
   hitColumns: {
     name: "Card Name", number: "Card Number", image: "Image URL",
     status: "Status", value: "Value", tier: "Tier",
